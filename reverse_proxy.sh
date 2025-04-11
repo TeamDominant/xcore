317b395ed5c6afd4f8d7a6d800b4dbce7e9a0c3e
cortez <89983388+cortez24rus@users.noreply.github.com>
2025-04-11 18:06:38 +0700

    0.7.1

--- reverse_proxy.sh
+++ reverse_proxy.sh
@@ -3,7 +3,7 @@
 ###################################
 ### Global values
 ###################################
-VERSION_MANAGER='0.7.0'
+VERSION_MANAGER='0.7.1'
 VERSION_XRAY='25.1.30'
 
 DIR_REVERSE_PROXY="/usr/local/reverse_proxy/"
