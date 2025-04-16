f152abac1b740bbd1a61e5fe4e628183adab347e
cortez <89983388+cortez24rus@users.noreply.github.com>
2025-04-17 00:45:45 +0700

    0.8.3

--- xcore.sh
+++ xcore.sh
@@ -3,7 +3,7 @@
 ###################################
 ### Global values
 ###################################
-VERSION_MANAGER='0.8.2'
+VERSION_MANAGER='0.8.3'
 VERSION_XRAY='25.1.30'
 
 DIR_XCORE="/opt/xcore"
