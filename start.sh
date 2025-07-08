a6710e747a1006eab05e1c0914646534350cf274
Adam <89983388+Adam-Sizzler@users.noreply.github.com>
2025-07-08 16:23:31 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ DIR_XCORE_PROXY="/opt/xcore"
 mkdir -p "${DIR_XCORE_PROXY}/repo/"
 
 TOKEN="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"
-REPO_URL="https://api.github.com/repos/cortez24rus/XCore/tarball/main"
+REPO_URL="https://api.github.com/repos/Adam-Sizzler/XCore/tarball/main"
 
 wget --header="Authorization: Bearer $TOKEN" -qO- $REPO_URL | tar xz --strip-components=1 -C "${DIR_XCORE_PROXY}/repo/"
 
